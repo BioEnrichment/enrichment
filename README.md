05bf0bd953a6b3020060bf8bc800ec659af22d58
James Alastair McLaughlin <james@clickteam.com>
2018-03-22 11:29:03 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+
+Link modules:
+
+    ./npm-link.sh
+
+Run everything in tmux:
+
+    gem install heytmux
+    tmux new-session "heytmux workspace.yml"
+
+
---
+++ config.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+red='\e[1;31m%s\e[0m\n'
+green='\e[1;32m%s\e[0m\n'
+
+printf "%s\n" "Checking for correct node version..."
+
+if [[ $(node --version) != v9* ]];
+then
+    printf "$red\n" "I need node v9.x.x"
+    exit 1
+else
+    printf "$green\n" "Found node v9.x.x"
+fi
+
+printf "Linking modules..."
+
+(cd ldf-facade && npm link)
+(cd eri-config && npm link)
+(cd eri-xrefdb && npm link)
+
+(cd eri-microservice-uniprot && npm install --link)
+(cd eri-microservice-rhea && npm install --link)
+(cd eri-microservice-chebi && npm install --link)
+
+
+
+if [ -z "$ENRICHMENT_CONFIG" ]; then
+    export ENRICHMENT_CONFIG=$(pwd)/enrichment.json
+fi
+
+rm -f env.sh
+printf "export %s=%s\n" "PATH" "$PATH" >> env.sh
+printf "export %s=%s\n" "ENRICHMENT_CONFIG" "$ENRICHMENT_CONFIG" >> env.sh
+
+
+
+
+
+
