2011fd58b65f1738dafc576a47bdb8cb98085576
James Alastair McLaughlin <james@clickteam.com>
2018-03-22 11:30:26 +0000

    Remove env.sh and add to .gitignore

--- env.sh
+++
@@ -1,2 +0,0 @@
-export PATH=/Users/james/.nvm/versions/node/v9.4.0/bin:/Users/james/cockatoo:/Users/james/android/tools:/Users/james/android/platform-tools:/Users/james/android/ndk:/usr/local/bin/clang/e1.30.0_64bit:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/go/bin:/usr/texbin:
-export ENRICHMENT_CONFIG=/Users/james/enrichment/enrichment.json
